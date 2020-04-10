21165fad9c62b777aec34c406f85d38d393c9f25
gxg0504 <gxg0504@163.com>
2020-04-10 23:37:32 +0800

    rm debug

--- base_rtos10.2-6 -裁剪 - FC0/MDK/debug/startup_lpc54608.d
+++
@@ -1,1 +0,0 @@
-debug\startup_lpc54608.o: ..\devices\LPC54608\arm\startup_LPC54608.s
--- base_rtos10.2-6 -裁剪 - FC0/MDK/debug/usb_host_ip3516hs.d
+++
@@ -1,2 +0,0 @@
-debug\usb_host_ip3516hs.o: ..\middleware\usb\host\usb_host_ip3516hs.c
-debug\usb_host_ip3516hs.o: ..\board\usb_host_config.h
